a74d7a66fd6147870b85a179f47e111783f4887d
Proxima11 <c14210139@john.petra.ac.id>
2023-05-15 09:48:45 +0700

    masih bug

--- 16Puzzle.py
+++ 16Puzzle.py
@@ -1,9 +1,9 @@
 import numpy
 
-def init_puzzle():
+def init_puzzle(puzzle_board):
     pass
 
-def solve_puzzle(puzzle_board):
+def solve_puzzle(puzzle_board, goal = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]]):
     #buat clone board
     temp_board = copy_board(puzzle_board)
     #cek posisi tukar
@@ -11,15 +11,19 @@ def solve_puzzle(puzzle_board):
     #batas tukar
     swap_board = []
     if (row-1) >= 0:
-        swap_board.append(swap_board(puzzle_board, row, col, row-1, col))
-    if (row+1) <= 2:
-        swap_board.append(swap_board(puzzle_board, row, col, row+1, col))
+        swap_board.append(swap(temp_board, row, col, row-1, col))
+    if (row+1) <= 3:
+        swap_board.append(swap(temp_board, row, col, row+1, col))
     if (col-1) >= 0:
-        swap_board.append(swap_board(puzzle_board, row, col, row, col-1))
-    if (col+1) <= 2:
-        swap_board.append(swap_board(puzzle_board, row, col, row, col+1))
-    #best score heuristic
-    
+        swap_board.append(swap(temp_board, row, col, row, col-1))
+    if (col+1) <= 3:
+        swap_board.append(swap(temp_board, row, col, row, col+1))
+    #best score heuristic (percent)
+    print(swap_board)
+    score = []
+    for i in swap_board:
+        score.append(heuristic(i, goal))
+    print(score)
     #loop lagi
 
 def find_blank(puzzle_board):
@@ -28,8 +32,14 @@ def find_blank(puzzle_board):
             if puzzle_board[i][j] == 0:
                 return i,j
             
-def heuristic(puzzle_board):
-    pass
+def heuristic(puzzle_board, goal):
+    matched = 0
+    for i in range(len(puzzle_board)):
+        for j in range(len(puzzle_board[i])):
+            if puzzle_board[i][j] == goal[i][j]:
+                matched += 1
+    return matched/16*100
+            
 
 def copy_board(puzzle_board):
     new_board = []
@@ -40,12 +50,12 @@ def copy_board(puzzle_board):
         new_board.append(row)
     return new_board
 
-def swap_board(puzzle_board, row, col, row_swap, col_swap):
+def swap(puzzle_board, row, col, row_swap, col_swap):
     puzzle_board[row][col], puzzle_board[row_swap][col_swap] = puzzle_board[row_swap][col_swap], puzzle_board[row][col]
     return puzzle_board
     
 
-puzzle_board = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]]
+puzzle_board = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,0,15]]
 
-puzzle_board = init_puzzle(puzzle_board)
+# puzzle_board = init_puzzle(puzzle_board)
 solve_puzzle(puzzle_board)
