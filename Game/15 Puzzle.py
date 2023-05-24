b643324d19322be6d8459e760322bb5e5d03d572
samuel <c14210181@john.petra.ac.id>
2023-05-24 09:01:08 +0700

    Update interface

--- Game/15 Puzzle.py
+++ Game/15 Puzzle.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 import time
 
 pygame.init()
@@ -15,15 +16,108 @@ tileImage = []
 board = [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]
 movement = [[None, None, None, None], [None, None, None, None], [None, None, None, None], [None, None, None, None]]
 
+# menu variables
+index1 = random.randint(0,15)
+index2 = random.randint(0,15)
+
+while index1 == index2:
+    index2 = random.randint(0,15)
+
+expand = True
+count = 0
+scaleX = 0
+scaleY = 0
+
+# game variables
+mouse_x = 0
+mouse_y = 0
 delay = 0.3
 last = 0
 
-# tile images
-full = pygame.image.load("16Puzzle/16Puzzle/Game/full.png").convert_alpha() #isi path sendiri
-for i in range (1,17):
-    tileImage.append(pygame.image.load(f"16Puzzle/16Puzzle/Game/tile{i}.png").convert_alpha()) #isi path sendiri
+hoverSolve = False
+hoverReset = False
+hoverShuffle = False
 
+# mode variables
+mainMenu = True
+startGame = False
+gameover = False
+
+# menu bg
+menuBg = pygame.image.load("mainMenuBg.png").convert_alpha()
+gameTitle = pygame.image.load("gameTitle.png").convert_alpha()
+startButton = pygame.image.load("startButton.png").convert_alpha()
+
+# tile images
+full = pygame.image.load("full.png").convert_alpha() #isi path sendiri
+for i in range (1,17):
+    tileImage.append(pygame.image.load(f"tile{i}.png").convert_alpha()) #isi path sendiri
+
+# ingame images
+inGameBg = pygame.image.load("backgroundGame.png").convert_alpha()
+header = pygame.image.load("headerGame.png").convert_alpha()
+solveUnhover = pygame.image.load("solveUnhover.png").convert_alpha()
+solveHover = pygame.image.load("solveHover.png").convert_alpha()
+resetUnhover = pygame.image.load("resetUnhover.png").convert_alpha()
+resetHover = pygame.image.load("resetHover.png").convert_alpha()
+shuffleUnhover = pygame.image.load("shuffleUnhover.png").convert_alpha()
+shuffleHover = pygame.image.load("shuffleHover.png").convert_alpha()
+
+def drawMenu():
+    screen.blit(menuBg, (0,0))
+    animateTile()
+    screen.blit(gameTitle, (0,0))
+    animateText()
+
+def animateTile():
+    image1 = pygame.transform.rotate(tileImage[index1], 20)
+    screen.blit(image1, (40,40))
+    image2 = pygame.transform.rotate(tileImage[index2], -30)
+    screen.blit(image2, (400,330))
+
+def animateText():
+    global expand, count, scaleX, scaleY
+
+    if expand:
+        scaleX+=0.9
+        scaleY+=0.1
+    else:
+        scaleX-=0.9
+        scaleY-=0.1
     
+    count+=1
+
+    if count==20:
+        if expand: expand = False
+        else : expand = True
+        count = 0
+
+    title = pygame.transform.scale(startButton, (startButton.get_width()+scaleX, startButton.get_height()+scaleY))
+    screen.blit(title, (110 ,550))
+
+# draw ingame backgrounds
+def drawGame():
+    screen.blit(inGameBg, (0,0))
+    screen.blit(header, (0,0))
+
+    drawIcons()
+
+# draw ingame action icons
+def drawIcons():
+    if hoverSolve:
+        screen.blit(solveHover, (337,48))
+    else:
+        screen.blit(solveUnhover, (337,50))
+
+    if hoverReset:
+        screen.blit(resetHover, (415,50))
+    else:
+        screen.blit(resetUnhover, (415,50))
+
+    if hoverShuffle:
+        screen.blit(shuffleHover, (499,50))
+    else:
+        screen.blit(shuffleUnhover, (499,50))
 
 # draw tile
 def drawTile():
@@ -190,9 +284,6 @@ def animationUp(tileNum, x, y):
     pygame.display.update()
 
 run = True
-menu = True
-play = False
-gameover = False
 
 #game play
 while run:
@@ -200,26 +291,77 @@ while run:
         if event.type == pygame.QUIT: run = False
     cclock = clock.tick(FPS)
 
-    drawTile()
-    enableMovement()
+    if mainMenu:
+        drawMenu()
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
 
-    key = pygame.key.get_pressed()
+        if click[0] and mouse_x >= 0 and mouse_y >= 0:
+            mainMenu = False
+            startGame = True
 
-    now = time.time()
+    elif startGame:
 
-    if key[pygame.K_RIGHT] and ((now-last) > delay):
-        last = now
-        moveRight()
-    elif key[pygame.K_LEFT] and ((now-last) > delay):
-        last = now
-        moveLeft()
-    elif key[pygame.K_UP] and ((now-last) > delay):
-        last = now
-        moveUp()
-    elif key[pygame.K_DOWN] and ((now-last) > delay):
-        last = now
-        moveDown()
-    
+        # time variables
+        now = time.time()
+
+        # draw bg and tiles
+        drawGame()
+        drawTile()
+        
+        # get keyboard input and enable movement
+        key = pygame.key.get_pressed()
+        enableMovement()
+
+        if key[pygame.K_RIGHT] and ((now-last) > delay):
+            last = now
+            moveRight()
+        elif key[pygame.K_LEFT] and ((now-last) > delay):
+            last = now
+            moveLeft()
+        elif key[pygame.K_UP] and ((now-last) > delay):
+            last = now
+            moveUp()
+        elif key[pygame.K_DOWN] and ((now-last) > delay):
+            last = now
+            moveDown()
+
+        # get mouse input
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
+        if (mouse_x >= 337 and mouse_x <= 337 + solveHover.get_width()) and (mouse_y >= 50 and mouse_y <= 50 + solveHover.get_height()):
+            hoverSolve = True
+        else:
+            hoverSolve = False
+
+        if (mouse_x >= 415 and mouse_x <= 415 + resetHover.get_width()) and (mouse_y >= 50 and mouse_y <= 50 + resetHover.get_height()):
+            hoverReset = True
+        else:
+            hoverReset = False
+
+        if (mouse_x >= 499 and mouse_x <= 499 + shuffleHover.get_width()) and (mouse_y >= 50 and mouse_y <= 50 + shuffleHover.get_height()):
+            hoverShuffle = True
+        else:
+            hoverShuffle = False
+
+        # enable button
+        click = pygame.mouse.get_pressed()
+
+        if click[0] and hoverSolve and ((now-last) > delay):
+            last = now
+            print("Solve")
+
+        if click[0] and hoverReset and ((now-last) > delay):
+            last = now
+            print("Reset")
+            
+        if click[0] and hoverShuffle and ((now-last) > delay):
+            last = now
+            print("Shuffle")
+
+
+    elif gameover:
+        pass
 
     pygame.display.update()
 
